781ce07af7bece51d2109947e6cdc3d982d21b6f
Shelgas <akozlov1997@gmail.com>
2024-06-07 19:07:40 +0300

    Added routing

---
+++ src/components/protected-route/protected-route.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+type ProtectedRouteProps = {
+  onlyUnAuth?: boolean;
+  children: React.ReactElement;
+};
+
+export const ProtectedRoute = ({
+  onlyUnAuth,
+  children
+}: ProtectedRouteProps) => {
+  onlyUnAuth = true;
+  return children;
+};
